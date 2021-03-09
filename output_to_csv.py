f6134d1b8f11100a415b4de084365239d72a7889
ahmsloahng <amlan.7770@gmail.com>
2021-03-10 02:00:49 +0530

    Code is added for printing output to csv/excel

---
+++ output_to_csv.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 10 01:28:57 2021
+
+@author: Amlan Ghosh
+"""
+import pandas as pd
+
+'''Define a dictionary with column names as key and empty list for values'''
+
+output_dict = {
+                 'Column1': [],
+                 'Column2': []
+              }
+
+'''Append the values in the list'''
+for values in iterator:
+    output_dict['Column1'].append(values)
+    output_dict['Column2'].append(values)
+
+temp_data = {
+            'Column1': output_dict['Column1'],
+            'Column2': output_dict['Column2']
+            }
+
+df = pd.DataFrame(temp_data, colums = ['Column1', 'Column2'])
+
+df.to_csv(path, index = False)
+df.to_excel(path, sheet_name = 'Sheet Name', index = False)
