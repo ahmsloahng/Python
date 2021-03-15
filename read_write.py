3ead9a6f32dfabd46661d14b70a76af0758146e8
ahmsloahng <amlan.7770@gmail.com>
2021-03-15 21:29:26 +0530

    Function to write in file is added

---
+++ read_write.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 15 21:19:08 2021
+
+@author: Amlan Ghosh
+"""
+
+def write():
+    h = open('File nanme', 'w')
+    h.write('Joy Mohun Bagan')
+    h.close()
+    
