c72c04cb5e7558554a8223fc3e73b3868ead1111
ahmsloahng <amlan.7770@gmail.com>
2021-04-01 21:54:46 +0530

    Reading excel file to dataframe is added

--- pandas.py
+++ pandas.py
@@ -4,12 +4,15 @@ Created on Wed Mar 10 01:38:57 2021
 
 @author: Amlan Ghosh
 """
+import pandas as pd
+
 class Pandas:
     
-    def __init__(self, dataframe, value, iterator):
+    def __init__(self, dataframe, value, iterator, excel):
         self.dataframe = dataframe
         self.value = value
         self.iterator = iterator
+        self.excel = excel
         
     ''' 1) Filtering subset of dataframe with respect to values in a column'''
     def subdataframe_singlevalue(self):
@@ -25,3 +28,7 @@ class Pandas:
     ''' 3) Creating dictionary from dataframe with two columns'''
     def create_dictionary(self):
         return self.dataframe.groupby(['Column1']).sum().to_dict['Column2'] #sums all the values corresponding to the values in Column1
+    
+    ''' 4) Reading from excel file and sheet'''
+    def excel_reading(self):
+        return self.pd.read_excel(self.excel, sheet_name = None)
