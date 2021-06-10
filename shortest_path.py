50b096e3bedbdb6f1fcfa2ea636d553a14d30282
ahmsloahng <amlan.7770@gmail.com>
2021-06-10 15:41:51 +0530

    Code updated for Djikstra's Algorithm

--- shortest_path.py
+++ shortest_path.py
@@ -9,13 +9,11 @@ Created on Thu Jun 10 11:32:10 2021
 def djikstra(dist_mat, nodes): #dist_mat: dictionary {node1: {node2: 2, node3: 4}, node2: {node1: 2}}
     opt_dist = [10**5]*nodes
     opt_dist[0] = 0
-    computed = [0]
-    visited = [False]*nodes
-    for i in computed:
-        if visited[i] == False:
-            for neighbour in dist_mat[i]:
-                opt_dist[neighbour] = min(opt_dist[neighbour], opt_dist[i] + dist_mat[i][neighbour])
-                if visited[neighbour] == False:
-                    computed.append(neighbour)
-            visited[i] = True
+    not_visited = list(i for i in range(25))
+    for i in range(nodes):
+        not_visited.sort(key = lambda x: opt_dist[x])
+        node = not_visited[0]
+        for neighbour in dist_mat[node]:
+            opt_dist[neighbour] = min(opt_dist[neighbour], opt_dist[node] + dist_mat[node][neighbour])
+        del not_visited[0]
     print (opt_dist)    
