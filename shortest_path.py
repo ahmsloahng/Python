b47bb278936783d7eb34f3bc9b75aa39d0e2d073
ahmsloahng <amlan.7770@gmail.com>
2021-06-17 14:43:34 +0530

    Djikstra's Algo is added with priority queue

--- shortest_path.py
+++ shortest_path.py
@@ -9,7 +9,7 @@ Created on Thu Jun 10 11:32:10 2021
 def djikstra(dist_mat, nodes): #dist_mat: dictionary {node1: {node2: 2, node3: 4}, node2: {node1: 2}}
     opt_dist = [10**5]*nodes
     opt_dist[0] = 0
-    not_visited = list(i for i in range(25))
+    not_visited = list(i for i in range(nodes))
     for i in range(nodes):
         node = not_visited[0]
         dist = opt_dist[node]
@@ -21,3 +21,24 @@ def djikstra(dist_mat, nodes): #dist_mat: dictionary {node1: {node2: 2, node3: 4
             opt_dist[neighbour] = min(opt_dist[neighbour], opt_dist[node] + dist_mat[node][neighbour])
         not_visited.remove(node)
     print (opt_dist)    
+
+'''Djikstra's Shortest Path using Priority Queue (heapq library in python)'''
+import heapq
+def djikstra_priorityqueue(dist_mat, nodes):
+    opt_dist = [10**5]*nodes
+    opt_dist[0] = 0
+    not_visited_nodes = [i for i in range(1,nodes)]
+    for neighbour in dist_mat[0]:
+        opt_dist[neighbour] = min(opt_dist[neighbour], opt_dist[0] + dist_mat[0][neighbour])
+    not_visited = [(opt_dist[i], i) for i in not_visited_nodes]
+    heapq.heapify(not_visited)
+    for i in range(nodes-1):
+        node = heapq.heappop(not_visited)
+        not_visited_nodes.remove(node[1])
+        for neighbour in dist_mat[node[1]]:
+            opt_dist[neighbour] = min(opt_dist[neighbour], opt_dist[node[1]] + dist_mat[node[1]][neighbour])
+        not_visited = [(opt_dist[i],i) for i in not_visited_nodes]
+        heapq.heapify(not_visited)
+    print (opt_dist)
+
+            
