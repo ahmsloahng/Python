d8708e2c1f99e164f2336ebcc9291b0d2e76f143
ahmsloahng <amlan.7770@gmail.com>
2025-07-05 21:11:05 +0530

    Quantile regression is added

--- machine_learning.py
+++ machine_learning.py
@@ -6,11 +6,31 @@ Created on Fri Jun 27 21:54:22 2025
 """
 
 '''Continuous Prediction Value
-    1. Regression
+    1. Regression -> numpy
+    2. Quantile regression -> statsmodel
 '''
 
 import numpy as np
 
-def numpy_regression(data):
+def regression(data):
+    
+    '''1. numpy'''
+    
     '''Input: a dataframe with x column and y column'''
     slope, intercept = np.ployfit(data['y'], data['x'])
+
+import statsmodel.formula.api as smf
+
+def quantile_regression(data, future_data):
+    
+    '''2. statsmodel'''
+    
+    '''Input: 
+        a dataframe with x1,x2 column and y column
+        a dataframe contains future dates with column x1 and x2
+    '''
+    quantile = 0.5 # quantile we need to predict
+    formula = 'y ~ x1 + x2' # formula defining the independent variables needed to be considered
+    model = smf.quantreg(formula, data) # define the model
+    result = model.fit(q = quantile) # fit the model
+    future = result.predict(future_data) # predict
