da30c6333f0234370b14db07d1046e397e8f32da
ahmsloahng <amlan.7770@gmail.com>
2025-11-20 13:12:38 +0530

    If any and if all is added

--- general.py
+++ general.py
@@ -17,3 +17,13 @@ def sort_dictionary(dic):
     sorted_items = sorted(dic.items(), key = lambda x:x[1])
     sorted_dic = {k:v for k,v in sorted_items}
     return sorted_dic
+
+'''If any item from list1 is present in list2 then true and enter the loop'''
+list1 = None
+list2 = None
+if any (term in list2 for term in list1):
+    condition = True
+
+'''If all term from list1 is present in list2 then true and enter the loop'''
+if all (term in list2 for term in list1):
+    condition = True
